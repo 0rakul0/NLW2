8a9e3e12198324da7196ef3124ce4a7daf01113d
0rakul0 <0rakul0render@gmail.com>
2020-08-03 12:40:07 -0300

    css landing
    css para mobile completo pag landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './assets/styles/global.css';
+import Landing from './pages/Landing'
 
 function App() {
   return (
-    <div className="App">
-      <h1>oi</h1>
-     
-    </div>
+    <Landing></Landing>
   );
 }
 
